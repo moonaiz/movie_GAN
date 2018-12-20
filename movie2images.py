8b8ef3a34e9b3554af9b74a62199d13138ebf8cb
moonaiz <takuya8495@gmail.com>
2018-12-20 17:07:27 +0900

    first commit

---
+++ movie2images.py
@@ -0,0 +1,55 @@
+import numpy as np
+import shutil
+import cv2
+import os
+
+def movie2image(input_dir, movie_name, output_dir):
+ part = 0
+ num = 0
+ video = cv2.VideoCapture(input_dir + movie_name)
+ while(video.isOpened()):
+    ret, frame = video.read()
+    frame_sum = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    if ret == False:
+        shutil.rmtree(output_dir + '/%s_part%s/'%(movie_name ,str(part)))
+        break
+
+    if num % 32 == 0:
+        part += 1
+
+    if not os.path.exists(output_dir + '/%s_part%s/'%(movie_name ,str(part))):
+        os.makedirs(output_dir + '/%s_part%s/'%(movie_name ,str(part)))
+
+    fix_frame = image_shape_fix(frame)
+
+    cv2.imwrite(output_dir + '/%s_part%s/%s.jpg'% (movie_name, str(part), str(num % 32)),fix_frame)
+    num += 1
+
+ video.release()
+
+def image_shape_fix(input_img):
+ img = input_img
+ if img.shape[0] > img.shape[1]:
+     long = img.shape[0]
+     short = img.shape[1]
+     fix_img = img[((long - short) // 2):((long - short)// 2 + short),:,:]
+ else:
+     long = img.shape[1]
+     short = img.shape[0]
+     fix_img = img[:, ((long - short) // 2):((long - short)// 2 + short),:]
+ reshape_img = cv2.resize(fix_img, dsize = (256, 256))
+ return reshape_img
+
+if __name__ == "__main__":
+ input_dir = './data/'
+ output_dir = './images/'
+ if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+ num = len(os.listdir(input_dir))
+
+ for i in range (num):
+
+    movie2image(input_dir, os.listdir(input_dir)[i], output_dir)
---
+++ pose_check.py
@@ -0,0 +1,95 @@
+import pose_utils
+import os
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter
+from skimage.draw import circle, line_aa, polygon
+import json
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from collections import defaultdict
+import skimage.measure, skimage.transform
+import sys
+import cv2
+
+
+LIMB_SEQ = [[1,2], [1,5], [2,3], [3,4], [5,6], [6,7], [1,8], [8,9],
+           [9,10], [1,11], [11,12], [12,13], [1,0], [0,14], [14,16],
+           [0,15], [15,17], [2,16], [5,17]]
+
+COLORS = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0],
+          [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255],
+          [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
+
+
+LABELS = ['nose', 'neck', 'Rsho', 'Relb', 'Rwri', 'Lsho', 'Lelb', 'Lwri',
+               'Rhip', 'Rkne', 'Rank', 'Lhip', 'Lkne', 'Lank', 'Leye', 'Reye', 'Lear', 'Rear']
+
+MISSING_VALUE = -1
+
+def load_pose_cords_from_strings(y_str, x_str):
+    y_cords = json.loads(y_str)
+    x_cords = json.loads(x_str)
+    return np.concatenate([np.expand_dims(y_cords, -1), np.expand_dims(x_cords, -1)], axis=1)
+
+def draw_pose_from_cords(pose_joints, img_size, radius=2, draw_joints=True):
+    colors = np.zeros(shape=img_size + (3, ), dtype=np.uint8)
+    mask = np.zeros(shape=img_size, dtype=bool)
+
+    if draw_joints:
+        for f, t in LIMB_SEQ:
+            from_missing = pose_joints[f][0] == MISSING_VALUE or pose_joints[f][1] == MISSING_VALUE
+            to_missing = pose_joints[t][0] == MISSING_VALUE or pose_joints[t][1] == MISSING_VALUE
+            if from_missing or to_missing:
+                continue
+            yy, xx, val = line_aa(pose_joints[f][0], pose_joints[f][1], pose_joints[t][0], pose_joints[t][1])
+            colors[yy, xx] = np.expand_dims(val, 1) * 255
+            mask[yy, xx] = True
+
+    for i, joint in enumerate(pose_joints):
+        if pose_joints[i][0] == MISSING_VALUE or pose_joints[i][1] == MISSING_VALUE:
+            continue
+        yy, xx = circle(joint[0], joint[1], radius=radius, shape=img_size)
+        colors[yy, xx] = COLORS[i]
+        mask[yy, xx] = True
+
+    return colors, mask
+
+if __name__ == "__main__":
+    import pandas as pd
+
+    args = args()
+    input_image_folder = './images/'
+    input_annotation_folder = './annotations/'
+    output_folder = './pose_check/'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    sub_folder  = '%s/' % kwargs['check_pose_dir']
+    if not os.path.exists(output_folder):
+        print('images folder is nothing')
+        exit()
+
+    images_list = os.listdir(input_image_folder + sub_folder)
+    for i in range(len(images_list))
+    output_path = output_folder + str(i) + '.jpg'
+
+    df = pd.read_csv(input_annotation_folder + sub_folder + '.csv', sep=':')
+
+    for index, row in df.iterrows():
+        pose_cords = load_pose_cords_from_strings(row['keypoints_y'], row['keypoints_x'])
+
+        colors, mask = draw_pose_from_cords(pose_cords, (128, 64))
+
+        mmm = produce_ma_mask(pose_cords, (128, 64)).astype(float)[..., np.newaxis].repeat(3, axis=-1)
+        print mmm.shape
+        img = imread(input_image_folder + sub_folder + row['name'])
+
+        mmm[mask] = colors[mask]
+
+        print (mmm)
+        plt.subplot(1, 1, 1)
+        plt.imshow(mmm)
+        plt.show()
