8b46d3f081f5b7a6a6a16923a5808089afeea4eb
moonaiz <takuya8495@gmail.com>
2019-02-01 17:02:49 +0900

    complete

--- pose_reforme.py
+++ pose_reforme.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import numpy as np
 import pandas as pd
 
 def load_pose_cords_from_strings(y_str, x_str):
@@ -7,10 +9,24 @@ def load_pose_cords_from_strings(y_str, x_str):
     return np.concatenate([np.expand_dims(y_cords, -1), np.expand_dims(x_cords, -1)], axis=1)
 
 def reforme_index(pose_cords, y_max, x_max, y_min, x_min):
-    for i in range(pose_cords.shape[0]):
-        for j in range(pose_cords.shape[1]):
-            y_cords = pose_cords[i][j][0]
-            x_cords = pose_cords[i][j][1]
+    y_cords = pose_cords[:,:,0]
+    x_cords = pose_cords[:,:,1]
+
+    if(y_max - y_min > x_max - x_min):
+        base_len = y_max - y_min
+    else:
+        base_len = x_max - x_min
+
+    #rescale 1 - 0
+    y_cords = (y_cords - y_min) / base_len
+    x_cords = (x_cords - x_min) / base_len
+
+    y_cords = y_cords * (image_size[0] - 2 * space)
+    y_cords += space
+    x_cords = x_cords * (image_size[0] - 2 * space)
+    x_cords += space
+
+    return y_cords, x_cords
 
 def min_max(pose_cords):
     y_max, x_max = 0, 0
@@ -28,7 +44,11 @@ def min_max(pose_cords):
 
 
 if __name__ == "__main__":
-    input_annotation = './output/annotations/'
+    from cmd import args
+
+    args = args()
+    input_annotation_folder = './handsup_annotations/'
+    image_size = args.image_size
 
     df = pd.read_csv(input_annotation_folder + '%s.csv'% args.check_pose_dir, sep=':')
 
@@ -38,4 +58,21 @@ if __name__ == "__main__":
             pose_cord = load_pose_cords_from_strings(row['keypoints_y'], row['keypoints_x'])
         pose_cords[i] = pose_cord
 
-    y_max, x_max, y_min, x_min = min_max(pose_cords)
+    (y_max, x_max, y_min, x_min) = min_max(pose_cords)
+
+    (y_cords, x_cords) = reforme_index(image_size, pose_cords, space, y_max, x_max, y_min, x_min)
+    pose_cords[:,:,0] = y_cords
+    pose_cords[:,:,1] = x_cords
+
+    pose_cords = pose_cords.astype(np.uint32)
+
+    output_path = input_annotation_folder + "%s-reforme.csv" %args.check_pose_dir
+
+    result_file = open(output_path, 'w')
+    processed_names = set()
+    print("name:keypoints_y:keypoints_x",file=result_file)
+
+    for t in range(32):
+        print('%s.jpg: %s: %s' % ('{0:02d}'.format(t), str(list(pose_cords[t, :, 0])), str(list(pose_cords[t, :, 1]))), file=result_file)
+
+    result_file.flush()
